f7e3612f68175674341f8bbee700ca60dd0c5559
huanghoujie18 <huanghoujie@163.com>
2020-07-19 15:17:31 +0800

    定义format方法

---
+++ test_requests/api/base_api1.py
@@ -0,0 +1,7 @@
+import json
+
+
+class BaseApi():
+    @classmethod  # 定义为类方法 ，无需实例化便可使用
+    def format(cls,r):
+        print(json.dumps(r.json(), indent=2))
---
+++ test_requests/testcase/test_base_api.py
@@ -0,0 +1,12 @@
+import requests
+from test_requests.api.base_api1 import BaseApi
+
+
+class TestBaseApi():
+    def test_format(self):
+        url =  'https://qyapi.weixin.qq.com/cgi-bin/gettoken'
+        corpid = 'ww0ff069f77ced0d7a'
+        secret = 'oKPwlYZ2bMunvnI4q6ZDr94YYZc_a4OxH2KM3oEh82k'
+        params = {'corpid':corpid, 'corpsecret':secret}  # 注意：不用self.corpid
+        r = requests.get(url,params=params)
+        BaseApi.format(r)  # 调用base_api1的format方法
