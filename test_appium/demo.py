680e1bdb58ef4552a84e62187a3df1f317a74fc7
huanghoujie18 <huanghoujie@163.com>
2020-09-26 14:51:28 +0800

    appium第一课

---
+++ test_appium/demo.py
@@ -0,0 +1,18 @@
+# This sample code uses the Appium python client
+# pip install Appium-Python-Client
+# Then you can paste this into a file and simply run with Python
+
+from appium import webdriver
+
+caps = {}
+caps["platformName"] = "Android"
+caps["deviceName"] = "test"
+
+driver = webdriver.Remote("http://localhost:4723/wd/hub", caps)
+driver.implicitly_wait(10)
+el1 = driver.find_element_by_id("com.xueqiu.android:id/tv_search")
+el1.click()
+el2 = driver.find_element_by_id("com.xueqiu.android:id/search_input_text")
+el2.send_keys("alibaba")
+
+driver.quit()
